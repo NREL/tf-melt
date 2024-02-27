929b94694bbb6be4ec069f2fd904bb7f06052ae8
nickwimer <nicholas.wimer@gmail.com>
2024-02-26 18:37:06 -0800

    extract base MELTModel from ANN and ResNet class

--- tfmelt/models.py
+++ tfmelt/models.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import Optional
 
 import tensorflow as tf
@@ -9,7 +10,7 @@ from tensorflow.keras.utils import register_keras_serializable
 
 
 @register_keras_serializable(package="tfmelt")
-class ArtificialNeuralNetwork(Model):
+class MELTModel(Model):
     def __init__(
         self,
         num_outputs: int,
@@ -19,15 +20,14 @@ class ArtificialNeuralNetwork(Model):
         dropout: Optional[float] = 0.0,
         input_dropout: Optional[float] = 0.0,
         batch_norm: Optional[bool] = False,
-        softmax: Optional[bool] = False,
-        sigmoid: Optional[bool] = False,
+        output_activation: Optional[str] = None,
         initializer: Optional[str] = "glorot_uniform",
         l1_reg: Optional[float] = 0.0,
         l2_reg: Optional[float] = 0.0,
         **kwargs,
     ):
         """
-        Artificial Neural Network model.
+        TF-MELT Base model.
 
         Args:
             num_outputs (int): Number of output units.
@@ -37,17 +37,14 @@ class ArtificialNeuralNetwork(Model):
             dropout (float, optional): Dropout rate for the hidden layers.
             input_dropout (float, optional): Dropout rate for the input layer.
             batch_norm (bool, optional): Whether to use batch normalization.
-            softmax (bool, optional): Whether to use softmax activation for the
-                                      output layer.
-            sigmoid (bool, optional): Whether to use sigmoid activation for the
-                                      output layer.
+            output_activation (str, optional): Activation function for the output layer.
             initializer (str, optional): Initializer for the weights.
             l1_reg (float, optional): L1 regularization for the weights.
             l2_reg (float, optional): L2 regularization for the weights.
             **kwargs: Additional keyword arguments.
 
         """
-        super(ArtificialNeuralNetwork, self).__init__(**kwargs)
+        super(MELTModel, self).__init__(**kwargs)
 
         self.num_outputs = num_outputs
         self.width = width
@@ -56,8 +53,7 @@ class ArtificialNeuralNetwork(Model):
         self.dropout = dropout
         self.input_dropout = input_dropout
         self.batch_norm = batch_norm
-        self.softmax = softmax
-        self.sigmoid = sigmoid
+        self.output_activation = output_activation
         self.initializer = initializer
         self.l1_reg = l1_reg
         self.l2_reg = l2_reg
@@ -76,30 +72,35 @@ class ArtificialNeuralNetwork(Model):
             "dropout": self.dropout,
             "input_dropout": self.input_dropout,
             "batch_norm": self.batch_norm,
-            "softmax": self.softmax,
-            "sigmoid": self.sigmoid,
+            "output_activation": self.output_activation,
             "initializer": self.initializer,
             "l1_reg": self.l1_reg,
             "l2_reg": self.l2_reg,
         }
 
-    def build(self, input_shape):
-        """Build the ANN."""
-        if self.softmax and self.sigmoid:
-            raise ValueError("Both softmax and sigmoid cannot be True.")
-        self.initialize_layers()
-        super(ArtificialNeuralNetwork, self).build(input_shape)
-
     def initialize_layers(self):
-        """Initialize the layers of the ANN."""
-        # Create regularizer
-        regularizer = (
+        """Initialize the layers of the model."""
+        self.create_regularizer()
+        self.create_dropout_layers()
+        self.create_batch_norm_layers()
+        self.create_input_layer()
+        self.create_output_layer()
+
+        # Set attribute flags based on which layers are present
+        self.has_batch_norm = hasattr(self, "batch_norm_layers")
+        self.has_dropout = hasattr(self, "dropout_layers")
+        self.has_input_dropout = hasattr(self, "input_dropout_layer")
+
+    def create_regularizer(self):
+        """Create the regularizer."""
+        self.regularizer = (
             regularizers.L1L2(l1=self.l1_reg, l2=self.l2_reg)
             if (self.l1_reg > 0 or self.l2_reg > 0)
             else None
         )
 
-        # Dropout layers
+    def create_dropout_layers(self):
+        """Create the dropout layers."""
         if self.dropout > 0:
             self.dropout_layers = [
                 Dropout(rate=self.dropout, name=f"dropout_{i}")
@@ -109,28 +110,79 @@ class ArtificialNeuralNetwork(Model):
             self.input_dropout_layer = Dropout(
                 rate=self.input_dropout, name="input_dropout"
             )
-        # Batch Normalization layers
+
+    def create_batch_norm_layers(self):
+        """Create the batch normalization layers."""
         if self.batch_norm:
             self.batch_norm_layers = [
                 BatchNormalization(name=f"batch_norm_{i}")
                 for i in range(self.depth + 1)
             ]
-        # One Dense layer connecting inputs to bulk layers
+
+    def create_input_layer(self):
+        """Create the input layer with associated activation layer."""
         self.dense_layer_in = Dense(
             self.width,
             activation=None,
             kernel_initializer=self.initializer,
-            kernel_regularizer=regularizer,
+            kernel_regularizer=self.regularizer,
             name="input2bulk",
         )
         self.activation_in = Activation(self.act_fun, name="input2bulk_act")
+
+    def create_output_layer(self):
+        """Create the output layer with activation from function."""
+        self.output_layer = Dense(
+            self.num_outputs,
+            activation=self.output_activation,
+            kernel_initializer=self.initializer,
+            kernel_regularizer=self.regularizer,
+            name="output",
+        )
+
+    def get_config(self):
+        """Get the config dictionary."""
+        config = super(MELTModel, self).get_config()
+        config.update(self.config)
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        """Create a model from a config dictionary."""
+        return cls(**config)
+
+
+@register_keras_serializable(package="tfmelt")
+class ArtificialNeuralNetwork(MELTModel):
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        """
+        Artificial Neural Network model.
+
+        Args:
+            **kwargs: Additional keyword arguments.
+
+        """
+        super(ArtificialNeuralNetwork, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        """Build the ANN."""
+        super(ArtificialNeuralNetwork, self).initialize_layers()
+        self.initialize_layers()
+        super(ArtificialNeuralNetwork, self).build(input_shape)
+
+    def initialize_layers(self):
+        """Initialize the layers of the ANN."""
+
         # Bulk layers
         self.dense_layers_bulk = [
             Dense(
                 self.width,
                 activation=None,
                 kernel_initializer=self.initializer,
-                kernel_regularizer=regularizer,
+                kernel_regularizer=self.regularizer,
                 name=f"bulk_{i}",
             )
             for i in range(self.depth)
@@ -138,34 +190,17 @@ class ArtificialNeuralNetwork(Model):
         self.activations_bulk = [
             Activation(self.act_fun, name=f"bulk_act_{i}") for i in range(self.depth)
         ]
-        # One Dense output layer with activation if requested
-        self.output_layer = Dense(
-            self.num_outputs,
-            activation=(self.get_ouput_activation()),
-            kernel_initializer=self.initializer,
-            kernel_regularizer=regularizer,
-            name="output",
-        )
-
-        # Set attribute flags based on which layers are present
-        self.has_batch_norm = hasattr(self, "batch_norm_layers")
-        self.has_dropout = hasattr(self, "dropout_layers")
-        self.has_input_dropout = hasattr(self, "input_dropout_layer")
-
-    def get_ouput_activation(self):
-        """Get the activation function for the output layer."""
-        return "softmax" if self.softmax else "sigmoid" if self.sigmoid else None
 
     @tf.function
     def call(self, inputs):
         """Call the ANN."""
-        # Apply input layer
+        # Apply input layer: dense -> batch norm -> activation -> input dropout
         x = self.dense_layer_in(inputs)
         x = self.batch_norm_layers[0](x) if self.has_batch_norm else x
         x = self.activation_in(x)
         x = self.input_dropout_layer(x) if self.has_input_dropout else x
 
-        # Apply bulk layers
+        # Apply bulk layers: dense -> batch norm -> activation -> dropout
         for i in range(self.depth):
             x = self.dense_layers_bulk[i](x)
             x = self.batch_norm_layers[i + 1](x) if self.has_batch_norm else x
@@ -175,34 +210,12 @@ class ArtificialNeuralNetwork(Model):
         # Return output layer output with activation built in
         return self.output_layer(x)
 
-    def get_config(self):
-        """Get the config dictionary."""
-        config = super(ArtificialNeuralNetwork, self).get_config()
-        config.update(self.config)
-        return config
-
-    @classmethod
-    def from_config(cls, config):
-        return cls(**config)
-
 
 @register_keras_serializable(package="tfmelt")
-class ResidualNeuralNetwork(Model):
+class ResidualNeuralNetwork(MELTModel):
     def __init__(
         self,
-        num_outputs: int,
-        width: Optional[int] = 32,
-        depth: Optional[int] = 2,
         layers_per_block: Optional[int] = 2,
-        act_fun: Optional[str] = "relu",
-        dropout: Optional[float] = 0.0,
-        input_dropout: Optional[float] = 0.0,
-        batch_norm: Optional[bool] = False,
-        softmax: Optional[bool] = False,
-        sigmoid: Optional[bool] = False,
-        initializer: Optional[str] = "glorot_uniform",
-        l1_reg: Optional[float] = 0.0,
-        l2_reg: Optional[float] = 0.0,
         pre_activation: Optional[bool] = True,
         post_add_activation: Optional[bool] = False,
         **kwargs,
@@ -211,19 +224,7 @@ class ResidualNeuralNetwork(Model):
         Initialize the ResidualNeuralNetwork model.
 
         Args:
-            num_outputs (int): Number of output units.
-            width (int, optional): Width of the network.
-            depth (int, optional): Depth of the network.
             layers_per_block (int, optional): Number of layers in each block.
-            act_fun (str, optional): Activation function to use.
-            dropout (float, optional): Dropout rate.
-            input_dropout (float, optional): Dropout rate for the input layer.
-            batch_norm (bool, optional): Whether to use batch normalization.
-            softmax (bool, optional): Whether to use softmax activation for the output layer
-            sigmoid (bool, optional): Whether to use sigmoid activation for the output layer.
-            initializer (str, optional): Weight initializer to use.
-            l1_reg (float, optional): L1 regularization strength.
-            l2_reg (float, optional): L2 regularization strength.
             pre_activation (bool, optional): Whether to use pre-activation in residual blocks.
             post_add_activation (bool, optional): Whether to apply activation after
                                                   adding the residual connection.
@@ -232,99 +233,40 @@ class ResidualNeuralNetwork(Model):
         """
         super(ResidualNeuralNetwork, self).__init__(**kwargs)
 
-        self.num_outputs = num_outputs
-        self.width = width
-        self.depth = depth
         self.layers_per_block = layers_per_block
-        self.act_fun = act_fun
-        self.dropout = dropout
-        self.input_dropout = input_dropout
-        self.batch_norm = batch_norm
-        self.softmax = softmax
-        self.sigmoid = sigmoid
-        self.initializer = initializer
-        self.l1_reg = l1_reg
-        self.l2_reg = l2_reg
         self.pre_activation = pre_activation
         self.post_add_activation = post_add_activation
 
-        # Initialize flags for layers (to be set in build method)
-        self.has_batch_norm = False
-        self.has_dropout = False
-        self.has_input_dropout = False
-
-        # Create config dictionary for serialization
-        self.config = {
-            "num_outputs": self.num_outputs,
-            "width": self.width,
-            "depth": self.depth,
-            "layers_per_block": self.layers_per_block,
-            "act_fun": self.act_fun,
-            "dropout": self.dropout,
-            "input_dropout": self.input_dropout,
-            "batch_norm": self.batch_norm,
-            "softmax": self.softmax,
-            "sigmoid": self.sigmoid,
-            "initializer": self.initializer,
-            "l1_reg": self.l1_reg,
-            "l2_reg": self.l2_reg,
-            "pre_activation": self.pre_activation,
-            "post_add_activation": self.post_add_activation,
-        }
+        # Update config with new attributes
+        self.config.update(
+            {
+                "layers_per_block": self.layers_per_block,
+                "pre_activation": self.pre_activation,
+                "post_add_activation": self.post_add_activation,
+            }
+        )
 
     def build(self, input_shape):
         """Build the ResNet."""
-        if self.softmax and self.sigmoid:
-            raise ValueError("Both softmax and sigmoid cannot be True.")
         if self.depth % self.layers_per_block != 0:
-            print(
+            warning.warn(
                 f"Warning: depth ({self.depth}) is not divisible by layers_per_block ({self.layers_per_block}), "
                 f"so the last block will have {self.depth % self.layers_per_block} layers."
             )
+        super(ResidualNeuralNetwork, self).initialize_layers()
         self.initialize_layers()
         super(ResidualNeuralNetwork, self).build(input_shape)
 
     def initialize_layers(self):
         """Initialize the layers of the ResNet."""
-        # Create regularizer
-        regularizer = (
-            regularizers.L1L2(l1=self.l1_reg, l2=self.l2_reg)
-            if (self.l1_reg > 0 or self.l2_reg > 0)
-            else None
-        )
 
-        # Dropout layers
-        if self.dropout > 0:
-            self.dropout_layers = [
-                Dropout(rate=self.dropout, name=f"dropout_{i}")
-                for i in range(self.depth)
-            ]
-        if self.input_dropout > 0:
-            self.input_dropout_layer = Dropout(
-                rate=self.input_dropout, name="input_dropout"
-            )
-        # Batch Normalization layers
-        if self.batch_norm:
-            self.batch_norm_layers = [
-                BatchNormalization(name=f"batch_norm_{i}")
-                for i in range(self.depth + 1)
-            ]
-        # One Dense layer connecting inputs to bulk layers
-        self.dense_layer_in = Dense(
-            self.width,
-            activation=None,
-            kernel_initializer=self.initializer,
-            kernel_regularizer=regularizer,
-            name="input2bulk",
-        )
-        self.activation_in = Activation(self.act_fun, name="input2bulk_act")
         # ResNet Bulk layers
         self.dense_layers_bulk = [
             Dense(
                 self.width,
                 activation=None,
                 kernel_initializer=self.initializer,
-                kernel_regularizer=regularizer,
+                kernel_regularizer=self.regularizer,
                 name=f"bulk_{i}",
             )
             for i in range(self.depth)
@@ -340,14 +282,6 @@ class ResidualNeuralNetwork(Model):
                 (self.depth + self.layers_per_block - 1) // self.layers_per_block
             )
         ]
-        # One Dense output layer with activation if requested
-        self.output_layer = Dense(
-            self.num_outputs,
-            activation=(self.get_ouput_activation()),
-            kernel_initializer=self.initializer,
-            kernel_regularizer=regularizer,
-            name="output",
-        )
         # Optional activation after the Add layers
         if self.post_add_activation:
             self.post_add_activations = [
@@ -355,19 +289,11 @@ class ResidualNeuralNetwork(Model):
                 for i in range(self.depth // 2)
             ]
 
-        # Set attribute flags based on which layers are present
-        self.has_batch_norm = hasattr(self, "batch_norm_layers")
-        self.has_dropout = hasattr(self, "dropout_layers")
-        self.has_input_dropout = hasattr(self, "input_dropout_layer")
-
-    def get_ouput_activation(self):
-        """Get the activation function for the output layer."""
-        return "softmax" if self.softmax else "sigmoid" if self.sigmoid else None
-
     @tf.function
     def call(self, inputs):
         """Call the ResNet."""
-        # Apply input layer
+        # Apply input layer:
+        # dense -> (pre-activation) -> batch norm -> input dropout -> (post-activation)
         x = self.dense_layer_in(inputs)
         x = self.activation_in(x) if self.pre_activation else x
         x = self.batch_norm_layers[0](x) if self.has_batch_norm else x
@@ -378,7 +304,8 @@ class ResidualNeuralNetwork(Model):
         for i in range(0, self.depth):
             y = x
 
-            # Apply bulk layers
+            # Apply bulk layer:
+            # dense -> (pre-activation) -> batch norm -> dropout -> (post-activation)
             x = self.dense_layers_bulk[i](x)
             x = self.activation_layers_bulk[i](x) if self.pre_activation else x
             x = self.batch_norm_layers[i + 1](x) if self.has_batch_norm else x
@@ -396,16 +323,6 @@ class ResidualNeuralNetwork(Model):
 
         # Return output layer output with activation built in
         return self.output_layer(x)
-
-    def get_config(self):
-        """Get the config dictionary."""
-        config = super(ArtificialNeuralNetwork, self).get_config()
-        config.update(self.config)
-        return config
-
-    @classmethod
-    def from_config(cls, config):
-        return cls(**config)
 
 
 @register_keras_serializable(package="tfmelt")
